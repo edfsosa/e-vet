ba9fb972ef905f484fd85d61f5271b18f90630a6
Edgar Franco <edfsosa@outlook.com>
2025-03-30 12:48:29 -0300

    Refactor AdminPanelProvider to add registration feature

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             ->passwordReset()
             ->spa()
             ->profile(isSimple: false)
